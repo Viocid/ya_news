67cb0cccd81deed81baa082e0692badc33270a98
Konstantin <153995261+Viocid@users.noreply.github.com>
2024-06-06 17:43:23 +0000

    Start

---
+++ news/tests/test_content.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.test import TestCase
+from news.models import News
+
+class TestContent(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        News.objects.bulk_create(
+            News(title=f'Новость {index}', text='Просто текст.')
+            for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
+        )
