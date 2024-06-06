607f905d50ae39a404e4bec18f629778ad31d3b2
Konstantin <versalione@gmail.com>
2024-06-06 22:32:35 +0300

    V1_1

--- news/tests/test_routes.py
+++ news/tests/test_routes.py
@@ -19,7 +19,8 @@ class TestRoutes(TestCase):
             news=cls.news,
             author=cls.author,
             text='Текст комментария'
-        ) 
+        )
+
     def test_pages_availability(self):
         urls = (
             ('news:home', None),
@@ -33,6 +34,20 @@ class TestRoutes(TestCase):
                 url = reverse(name, args=args)
                 response = self.client.get(url)
                 self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_availability_for_comment_edit_and_delete(self):
+        users_statuses = (
+            (self.author, HTTPStatus.OK),
+            (self.reader, HTTPStatus.NOT_FOUND),
+        )
+        for user, status in users_statuses:
+            self.client.force_login(user)
+            for name in ('news:edit', 'news:delete'):
+                with self.subTest(user=user, name=name):
+                    url = reverse(name, args=(self.comment.id,))
+                    response = self.client.get(url)
+                    self.assertEqual(response.status_code, status)
+
     def test_redirect_for_anonymous_client(self):
         login_url = reverse('users:login')
         for name in ('news:edit', 'news:delete'):
@@ -40,14 +55,6 @@ class TestRoutes(TestCase):
                 
                 url = reverse(name, args=(self.comment.id,))
 
-                redirect_url = f'{login_url}?next={url}'
-                response = self.client.get(url)
-                self.assertRedirects(response, redirect_url)
-    def test_redirect_for_anonymous_client(self):
-        login_url = reverse('users:login')
-        for name in ('news:edit', 'news:delete'):
-            with self.subTest(name=name):
-                url = reverse(name, args=(self.comment.id,))
                 redirect_url = f'{login_url}?next={url}'
                 response = self.client.get(url)
                 self.assertRedirects(response, redirect_url)
