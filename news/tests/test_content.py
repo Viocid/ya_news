33350784919e08d0bd569c41c98a3a52a11b4f40
Konstantin <153995261+Viocid@users.noreply.github.com>
2024-06-08 17:55:28 +0000

    Content_replit

--- news/tests/test_content.py
+++ news/tests/test_content.py
@@ -1,11 +1,38 @@
 from django.conf import settings
 from django.test import TestCase
 from news.models import News
+from django.urls import reverse
+from datetime import datetime, timedelta
+
 
 class TestContent(TestCase):
+    HOME_URL = reverse('news:home')
+
+
     @classmethod
     def setUpTestData(cls):
-        News.objects.bulk_create(
-            News(title=f'Новость {index}', text='Просто текст.')
+        today = datetime.today()
+        all_news = [
+            News(
+                title=f'Новость {index}',
+                text='Просто текст.',
+                date=today - timedelta(days=index)
+            )
             for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
-        )
+        ]
+        News.objects.bulk_create(all_news) 
+
+
+    def test_news_count(self):
+        response = self.client.get(self.HOME_URL)
+        object_list = response.context['object_list']
+        news_count = object_list.count()
+        self.assertEqual(news_count, settings.NEWS_COUNT_ON_HOME_PAGE) 
+
+
+    def test_news_order(self):
+        response = self.client.get(self.HOME_URL)
+        object_list = response.context['object_list']
+        all_dates = [news.date for news in object_list]
+        sorted_dates = sorted(all_dates, reverse=True)
+        self.assertEqual(all_dates, sorted_dates) 
